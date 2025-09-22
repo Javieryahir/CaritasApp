83346da199408bc7d9b12dda7bb897e205c409ff
Javieryahir <javier.juarez@lexbuddy.ai>
2025-09-22 05:11:36 -0600

    Add sNavigation

--- app/src/main/java/com/example/caritasapp/LoginScreen.kt
+++ app/src/main/java/com/example/caritasapp/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.caritasapp
 
+import MapScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,11 +20,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navController: NavController) {
+
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -58,7 +65,7 @@ fun LoginScreen() {
 
         // Botón Google
         Button(
-            onClick = { /* TODO: acción login */ },
+            onClick = {navController.navigate("search") },
             shape = RoundedCornerShape(50),
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.White,
@@ -95,8 +102,3 @@ fun LoginScreen() {
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun PreviewLoginScreen() {
-    LoginScreen()
-}
--- app/src/main/java/com/example/caritasapp/MainActivity.kt
+++ app/src/main/java/com/example/caritasapp/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.caritasapp
 
+import MapScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.caritasapp.LoginScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
-            ShelterDetailsScreen()
+            MyApp()
         }
     }
 }
 
 
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }      // ← pantalla Login
+        composable("search") { MapScreen(navController) }       // ← pantalla Mapa
+        composable("shelter") { ShelterDetailsScreen(navController) } // ← pantalla detalles
+    }
+}
 
